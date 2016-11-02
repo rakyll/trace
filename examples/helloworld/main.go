a461c96ab7b34382ce59ea40e1c5bd1ca717ed03
Jaana Burcu Dogan <jbd@google.com>
2016-11-02 00:52:01 -0700

    rewrite

--- examples/helloworld/main.go
+++ examples/helloworld/main.go
@@ -2,36 +2,44 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/rakyll/trace"
 )
 
+var t *trace.Client
+
 func main() {
-	f(context.TODO())
+	ctx := context.Background()
+
+	client, err := trace.NewClient(ctx, "bamboo-shift-504")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t = client
+	f(ctx)
 }
 
 func f(ctx context.Context) {
-	ctx, s := trace.NewSpan(ctx, "f")
+	ctx, s := t.NewSpan(ctx, "f")
 	defer s.Finish()
 
 	a1(ctx)
 	a2(ctx)
 	a3(ctx)
-
-	trace.Log()
 }
 
 func a1(ctx context.Context) {
-	ctx, s := trace.NewSpan(ctx, "a")
+	ctx, s := t.NewSpan(ctx, "a")
 	defer s.Finish()
 }
 
 func a2(ctx context.Context) {
-	ctx, s := trace.NewSpan(ctx, "a")
+	ctx, s := t.NewSpan(ctx, "a")
 	defer s.Finish()
 }
 
 func a3(ctx context.Context) {
-	ctx, s := trace.NewSpan(ctx, "a")
+	ctx, s := t.NewSpan(ctx, "a")
 	defer s.Finish()
 }
