5e2949098b8995e470eebff67a2d7d90a576a98f
Jaana Burcu Dogan <jbd@google.com>
2017-03-01 18:17:45 -0800

    fix the package example

--- examples_test.go
+++ examples_test.go
@@ -1,28 +1,31 @@
 package trace_test
 
 import (
+	"context"
 	"log"
+	"net/http"
 
 	"github.com/rakyll/trace"
 	"github.com/rakyll/trace/gcp"
 )
 
 func ExampleNewTrace() {
-	c, err := gcp.NewClient("project-id")
+	c, err := gcp.NewClient(context.Background(), "project-id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	trace.Configure(c)
+
+	span, finish := trace.NewSpan("/foo")
+	defer finish()
+
+	span.SetLabel("hello", []byte("error happened"))
+
+	req, _ := http.NewRequest("GET", "http://google.com/", nil)
+	req, err = span.ToHTTPReq(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	root := trace.NewTrace(c)
-	sp, finish := root.Child("/whatever")
-	defer finish()
-
-	sp.SetLabel("hello", []byte("error happened"))
-
-	trace.ToHTTP(c, req, sp)
-
-	carrier, ok := c.(HTTPCarrier)
-	if ok {
-		carrier.ToHTTP(req, sp.ID())
-	}
+	// do the request
 }
