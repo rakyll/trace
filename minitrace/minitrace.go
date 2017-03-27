9abd8ce65fcc39e6c1cee4451abcb7df028a57e9
Jaana Burcu Dogan <jbd@google.com>
2017-03-27 01:57:08 -0700

    initial commit of minitrace

---
+++ minitrace/minitrace.go
@@ -0,0 +1,25 @@
+package minitrace
+
+import (
+	"context"
+	"net/http"
+)
+
+type Span []byte
+
+func NewContext(ctx context.Context, s Span) context.Context {
+	return context.WithValue(ctx, spanKey, s)
+}
+
+func FromContext(ctx context.Context) Span {
+	return ctx.Value(spanKey).(Span)
+}
+
+type ReqCarrier interface {
+	SpanFromReq(req *http.Request) (Span, error)
+	SpanToReq(req *http.Request, s Span) error
+}
+
+type contextKey struct{}
+
+var spanKey = contextKey{}
