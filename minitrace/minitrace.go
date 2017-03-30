9fba5f09b387c440f4bc46b5d227c2282d9301a1
Jaana Burcu Dogan <jbd@google.com>
2017-03-30 01:35:50 -0700

    add godoc

--- minitrace/minitrace.go
+++ minitrace/minitrace.go
@@ -1,3 +1,5 @@
+// Package minitrace contains primitives to support
+// propagation of tracing information.
 package minitrace
 
 import (
@@ -5,16 +7,30 @@ import (
 	"net/http"
 )
 
+// Span identifies a span globally in a tracing system
+// and may contain annotations the span is propagating.
+//
+// The identification and the formatting of propagated
+// annotations might be tracing backend-specific.
 type Span []byte
 
+// NewContext returns a context with the given span.
 func NewContext(ctx context.Context, s Span) context.Context {
 	return context.WithValue(ctx, spanKey, s)
 }
 
+// FromContext returns the span from the context.
 func FromContext(ctx context.Context) Span {
 	return ctx.Value(spanKey).(Span)
 }
 
+// HTTPCarrier allows spans to be propagated via HTTP requests.
+//
+// SpanFromReq returns a span from the incoming HTTP request.
+// If the incoming request doesn't contain trace information,
+// a nil span is returned with no errors.
+//
+// SpanToReq mutates the outgoing request with the span.
 type HTTPCarrier interface {
 	SpanFromReq(req *http.Request) (Span, error)
 	SpanToReq(req *http.Request, s Span) error
